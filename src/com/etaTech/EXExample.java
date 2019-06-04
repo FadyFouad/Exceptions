f048a56dc772173e90aad37c60d4f7f7eade266c
fadyfouad <fady.fouad.a@gmail.com>
2019-06-05 00:13:14 +0200

    handle exception

--- src/com/etaTech/EXExample.java
+++ src/com/etaTech/EXExample.java
@@ -1,6 +1,7 @@
 package com.etaTech;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /****************************************************
@@ -13,20 +14,28 @@ public class EXExample {
     }
 
     private static int divide() {
-        int x = getInt();
-        int y = getInt();
-        return x/y;
+        int x;
+        int y;
+        try {
+            x = getInt();
+            y = getInt();
+            return x / y;
+        } catch (NoSuchElementException e) {
+            throw new NoSuchElementException("No Suitable input");
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("Cant divide By Zero");
+        }
     }
 
     private static int getInt() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number");
-        while(true){
-            try{
+        while (true) {
+            try {
                 return scanner.nextInt();
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 scanner.nextLine();
-                System.out.println("Exception : "+e.getMessage());
+                System.out.println("Exception : " + e.getMessage());
                 System.out.println("Enter Number 0:9");
             }
         }
